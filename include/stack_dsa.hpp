372cc39d6381d171f70aecb7cb067eaf00b3a4ee
lil-brumski <davidbrume7@gmail.com>
2025-10-22 13:47:05 +0100

    Added stack data structure, but will implement later.

---
+++ include/stack_dsa.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <linkedlist.hpp>
+
+/**
+* 
+*/
+void UsingStack();
+
+/**
+*
+*/
+template<class T>
+class MyStack : public LinkedList<T> {
+public:
+	/**
+	* Allows MyStack class to use the base class's constructors
+	*/
+	using LinkedList<T>::LinkedList; 
+
+private:
+	DSAPTR<Node<T>> myTop; /**< Pointer to the top element of the stack*/
+
+public:
+	/**
+	* Returns the top element of the stack
+	*/
+	T Top() {
+
+	}
+
+	/**
+	* Pushes a new element to the top of the stack
+	*/
+	MyStack<T> push_top() {
+		return *this;
+	}
+
+	/**
+	* Removes the top element of the stack
+	*/
+	MyStack<T> popTop() {
+		return *this;
+	}
+
+	/**
+	* Returns true if the stack is empty
+	*/
+	bool isEmpty() {
+		if (myTop) return true;
+		else return false;
+
+		return false;
+	}
+
+	/**
+	* Deleted copy assignment operator
+	*/
+	MyStack<T>& operator=(const MyStack<T>&) = delete;
+
+	/**
+	* Default moves assignment operator
+	*/
+	MyStack<T>& operator=(MyStack<T>&&) = default;
+};
---
+++ src/stack_dsa.cpp
@@ -0,0 +1,11 @@
+#include <stack_dsa.hpp>
+#include <stack>
+
+void UsingStack() {
+	std::stack<int> myStackInt;
+	myStackInt.push(20);
+	myStackInt.push(40);
+	myStackInt.push(60);
+	myStackInt.push(80);
+	myStackInt.push(100);
+}
